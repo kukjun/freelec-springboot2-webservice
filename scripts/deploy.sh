9ab51f8d0d0d3cc736b9b183199231116211f678
Kukjun Lee <20171655@edu.hanbat.ac.kr>
2022-06-04 17:17:11 +0900

    <fix> : <properties 확장자 오류>
    - properties 파일을 yml 파일로 변경
    
    Resolves: #issueNo
    See also: #issueNo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,6 +34,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-  -Dspring.config.location=classPath:/application.properties,classpath:/application-ral.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+  -Dspring.config.location=classPath:/application.properties,classpath:/application-ral.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
   -Dspring.profiles.active=real \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
