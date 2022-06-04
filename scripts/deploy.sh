1b5999ab300805557e821ef7e32f7ea69c2fa483
Kukjun Lee <20171655@edu.hanbat.ac.kr>
2022-06-04 16:54:36 +0900

    <fix> : <deploy.sh 오타 수정>
    - gradle 버전 업그레이드로 인해 plain.jar이 생성되어서, 해당 내용을 받지 않도록 수정
    - $ 오타 -> & 으로 수정
    
    Resolves: #issueNo
    See also: #issueNo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,7 +23,7 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
@@ -36,4 +36,4 @@ echo "> $JAR_NAME 실행"
 nohup java -jar \
   -Dspring.config.location=classPath:/application.properties,classpath:/application-ral.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>$1 $
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
