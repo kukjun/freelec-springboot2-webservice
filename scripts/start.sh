ab9482ad8087f79dba638985371d3b0da099c86a
Kukjun Lee <lxx3380@gmail.com>
2022-06-05 16:21:19 +0900

    <fix> : <오타로 인해 .sh 내용 수정>
    - 자동완성 기능과 코드 정렬 도중 원하는 문법에 어긋나는 경우가 발생.
    
    Resolves: #issueNo
    See also: #issueNo

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,6 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
-
 PROJECT_NAME=freelec-springboot2-webservice
 
 echo "> Build 파일 복사"
@@ -30,4 +29,4 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
   -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
   -Dspring.profiles.active=$IDLE_PROFILE \
-  $JAR_NAME >$REPOSITORY/nohup.out 2>&1 &
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
